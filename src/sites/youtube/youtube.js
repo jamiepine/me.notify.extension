8728ca1f473201685c6896794b1aaede91eb130b
Timothy Cole <tim@timcole.me>
2018-07-31 22:36:19 -0400

    Added notify.me button that alerts the channelid (btn selector is wrong)

--- src/sites/youtube/youtube.js
+++ src/sites/youtube/youtube.js
@@ -1,65 +1,48 @@
 /* eslint-disable */
+'use strict';
 
+(function() {
+  let channelID = () => {
+    var channelName = '';
 
+    const watchPage = document.querySelector(`#owner-name a`);
+    if (watchPage !== '') {
+      channelName = watchPage.href;
+      var len = channelName.length;
+      channelName = channelName.substr(len - 24, len);
+    }
 
+    return channelName;
+  };
 
-// document.addEventListener("DOMContentLoaded", (e) => { 
+  const watchPage = () => {
+    const videoMeta = document.getElementById(`meta`);
+    const subscribeButtonSection = videoMeta.parentNode.querySelector(`#meta-contents #subscribe-button`);
+    const subscribeButton = subscribeButtonSection.childNodes[0];
 
-    
-// })
-// document.body.style.display = 'none'
+    document.querySelector(`#subscribe-button.ytd-video-secondary-info-renderer`).style.flexDirection = 'row';
 
-//  setTimeout(() => {
+    const bellButton = document.createElement('paper-button');
+    bellButton.classList = 'style-scope ytd-button-renderer style-default';
+    bellButton.id = 'notify-bell';
+    bellButton.setAttribute('elevation', '0');
+    bellButton.setAttribute('aria-disabled', 'false');
+    bellButton.setAttribute('tabindex', '0');
+    bellButton.setAttribute('role', 'button');
+    bellButton.innerText = `Notify.me`;
+    bellButton.style = 'color: #B4DA55; border: 1px solid #B4DA55; width: 75px; border-radius: 3px; margin: 7px;';
 
+    bellButton.onclick = function() {
+      alert(channelID());
+    };
 
+    subscribeButtonSection.insertBefore(bellButton, subscribeButton);
+  };
 
-//  }, 1000)
+  const embeded = () => {
+    if (window.location.pathname === '/watch') watchPage();
+  };
 
-// if (document.readyState === 'complete') {
-//     var loolsubButton = document.getElementById('subscribe-button');
-//     lo0=lsubButton.style.display = 'none'
-// }
-// document.addEventListener('DOMContentLoaded', (e) => { 
-//     const subButton = document.getElementById('subscribe-button')
-//     subButton.style.display = 'none'
-
-//  })
-
-
-// document.body.addEventListener("yt-navigate-finish", (e) => { 
-//     if (!subButton || subButton.querySelector) return
-
-//     var subButton = document.querySelector('.style-scope.ytd-video-secondary-info-renderer#subscribe-button')
-    
-//     subButton.style.display = 'none'
-
-//  })
-
-
-
-
- document.addEventListener("yt-navigate-finish", (e) => { 
-
-    var jeff = document.getElementById('logo')
-    jeff.style.display = 'none'
-
-
-        // var subBtn = document.querySelectorAll("[id='subscribe-button']")[1]; 
-        // var paperBtn = document.createElement('paper-button'); 
-        // subBtn.insertBefore(paperBtn, subBtn.firstChild); 
-
-
-        // setInterval(() => {
-        //     if (document.readyState != 'complete') return;
-        //     const subButton = document.querySelector('.style-scope.ytd-video-secondary-info-renderer#subscribe-button');
-        //     subButton.style.display = 'none';
-        // }, 100)
-
-        
-            const b = document.querySelector('.style-scope.ytd-video-secondary-info-renderer#subscribe-button'); 
-            b.style.display = "none";
-     
-
-});
-
-
+  embeded();
+  document.addEventListener('yt-navigate-finish', embeded);
+})();
